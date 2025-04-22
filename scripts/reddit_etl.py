bf0d618a76c422759eda9a2ce37fa13bb5ab281a
brian <brianotina20@gmail.com>
2025-04-22 14:20:32 +0300

    ky

--- scripts/reddit_etl.py
+++ scripts/reddit_etl.py
@@ -2,18 +2,23 @@ import praw, os
 import pandas as pd
 
 from datetime import datetime, timezone
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 def run_reddit_etl():
 
     """
     Function to extract data from Reddit and load it into a CSV file.
     """
+    load_dotenv()
+
+    client_id_reddit = os.getenv("CLIENT_ID_REDDIT")
+    client_secret_reddit = os.getenv("CLIENT_SECRET_REDDIT")
+    user_agent_reddit = os.getenv("USER_AGENT_REDDIT")
 
     reddit = praw.Reddit(
-        client_id="Bi4xfiEOE9H8BS9YDmXZmw",
-        client_secret="kdoUUfXYo3JyJjv_x8KvAquQ6yhNCA",
-        user_agent="etl_redit"
+        client_id=client_id_reddit,
+        client_secret=client_secret_reddit,
+        user_agent= user_agent_reddit
     )
 
     subreddit = reddit.subreddit("espresso")
